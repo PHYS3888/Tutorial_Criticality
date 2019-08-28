e4946781571a43a5e04165ac52ce72656a7b0ade
Bmunn <bmun7133@uni.sydney.edu.au>
2019-08-28 17:05:47 +1000

    Update variable name s to treeBurn

--- fireArea.m
+++ fireArea.m
@@ -1,10 +1,10 @@
-function s = fireArea(data)
+function treeBurn = fireArea(data)
 
 % Find connected clusters nearest neighbours in succesive timesteps
 CC = bwconncomp(data==2,18);
 
 % Find number of trees within a connected cluster
 SS = regionprops(CC,'Area');
-s = cat(1,SS.Area);
+treeBurn = cat(1,SS.Area);
 
 end
